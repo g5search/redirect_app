f4fa6f3bc2106cb83a62f078565b9f44550f438b
Tyler Hasenoehrl <tylrhas@gmail.com>
2018-09-28 17:32:11 -0700

    add some error handling

--- server.js
+++ server.js
@@ -19,19 +19,24 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 async function redirectApp(req, res) {
     var path = req.path
-    var domain = req.headers.host
+    var host = req.headers.host
     var protocol = req.protocol
     console.log('vhost for', req.headers.host);
     // query the database for the url and its destination
-    var redirect = await getRedirect(protocol, domain, path)
-    res.redirect(redirect)
+    var redirect = await getRedirect(protocol, host, path)
+    if ('destination' in redirect) {
+        res.redirect(redirect.destination)
+    } else {
+        // send error
+        res.send(redirect.error)
+    }
 }
 
-async function getRedirect(protocol, domain, path) {
+async function getRedirect(protocol, host, path) {
     // look for domain and path 
-    var redirect = await models.domain.findAll({
+    var domain = await models.domain.findAll({
         where: {
-            domain: domain
+            domain: host
         },
         include: [
             {
@@ -42,5 +47,31 @@ async function getRedirect(protocol, domain, path) {
             }
         ]
     })
-    // if the domain is found but there is no path just upgrade the protocol to https
+    if (domain.length > 1) {
+        return await formatRedirect(domain[0])
+    } else {
+        return { error: 'There is no redirect for this domain'}
+    }
+    // if the domain is found but there is no path just upgrade the protocol to https and redirect to www
+}
+
+async function formatRedirect (domain) {
+    if (domain.redirects.length > 1) {
+        // there is more than one redirect for the domain and path
+        return { error: 'more than one redirect for this host and path'}
+    } else if (domain.redirects.length === 1) {
+        var redirect = domain.redirects[0]
+        // there is only one redirect for this domain and path
+        let destination = redirect.destination
+        let protocol = null
+        if (redirect.secure_destination === true) {
+            destination = 'https://' + destination
+        } else {
+            destination = 'http://' + destination
+        }
+        return { destination }
+    } else {
+        // there is no redirect for this 
+        return { error: 'There is no redirect for this domain'}
+    }
 }
