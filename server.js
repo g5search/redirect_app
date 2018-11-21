ebd305050c031d6727fc531e2049ccda6c2af538
Kyle Rose <kyle.rose@getg5.com>
2018-11-21 13:43:35 -0800

    es6 -refactor

--- server.js
+++ server.js
@@ -1,32 +1,26 @@
 require('dotenv').config()
 var models = require('./app/models')
 
-//Sync the Database
-models.sequelize.sync().then(function () {
-  console.log('Nice! Database looks fine')
-  
-  // start the servers only if you are connected to a database
-  var server = glx.listen(80, 443);
-  server.on('listening', function () {
-    console.info(server.type + " listening on", server.address());
-  });
+const { GREENLOCK_SERVER: server, GREENLOCK_DIR: configDir, GREENLOCK_EMAIL: email, GREENLOCK_AGREETOS: agreeTos, GREENLOCK_COMMUNITYMEMBER: communityMember, GREENLOCK_DEBUG: debug } = process.env
 
-}).catch(function (err) {
-  console.log(err, "Something went wrong with the Database Connection!")
-});
+//Sync the Database
+models.sequelize.sync().then(_ => {
+  const server = glx.listen(80, 443);
+  server.on('listening', _ => console.info(`${server.type} listening on ${server.address()}`));
+}).catch(console.error);
 
 var glx = require('greenlock-express').create({
   version: 'draft-11',                                // Let's Encrypt v2 is ACME draft 11
-  server: process.env.GREENLOCK_SERVER,              // If at first you don't succeed, stop and switch to staging
-  configDir: process.env.GREENLOCK_DIR,             // You MUST have access to write to directory where certs are saved.
-  approveDomains: approveDomains,                  // Greenlock's wraps around tls.SNICallback. Check the domain name here and reject invalid ones
-  app: function (req, res) {
+  server,              // If at first you don't succeed, stop and switch to staging
+  configDir,             // You MUST have access to write to directory where certs are saved.
+  approveDomains,                  // Greenlock's wraps around tls.SNICallback. Check the domain name here and reject invalid ones
+  app: (req, res) => {
     require('./app/lib/index.js')(req, res)
   },
-  email: process.env.GREENLOCK_EMAIL,                                     // Email for Let's Encrypt account and Greenlock Security
-  agreeTos: (process.env.GREENLOCK_AGREETOS == "true"),                  // Accept Let's Encrypt ToS
-  communityMember: (process.env.GREENLOCK_COMMUNITYMEMBER == "true"),   // Join Greenlock to get important updates, no spam
-  debug: (process.env.GREENLOCK_DEBUG == "true")
+  email,                                     // Email for Let's Encrypt account and Greenlock Security
+  agreeTos: (agreeTos == 'true'),                  // Accept Let's Encrypt ToS
+  communityMember: (communityMember == 'true'),   // Join Greenlock to get important updates, no spam
+  debug: (debug == 'true')
 });
 
 // [SECURITY]
@@ -34,20 +28,11 @@ var glx = require('greenlock-express').create({
 // SQL injection and timing attacks by rejecting invalid domain names,
 // but it's up to you to make sure that you accept opts.domain BEFORE
 // an attempt is made to issue a certificate for it.
-async function approveDomains(opts, certs, cb) {
-
+async function approveDomains(options, certs, cb) {
   // Check that the hosting domain exists in the database.
-  var domain = await models.domain.findAll({
-    where: {
-      domain: opts.domain
-    }
-  })
-  if (domain.length > 0) {
-    // the domain is in the database proceed
-    cb(null, { options: opts, certs: certs });
-  } else {
-    // error callback
-    cb(new Error('no config found for ' + opts.domain))
-    return;
-  }
+  (await models.domain.findAll({
+    where: { domain: options.domain }
+  })).length ?
+    cb(null, { options, certs }) :
+    cb(new Error(`no config found for ${options.domain}`));
 }
