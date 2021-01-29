514970728637381619caed782e875d78ba90c324
Tyler Hasenoehrl <tylrhas@gmail.com>
2021-01-28 16:46:16 -0800

    formatting

--- server.js
+++ server.js
@@ -2,26 +2,28 @@ require('dotenv').config()
 const models = require('./app/models')
 const app = require('./app/lib/index.js')
 var pkg = require("./package.json");
+
 // Sync the Database
 models.sequelize
   .sync()
   .then(() => {
-    require("greenlock-express")
-    .init({
-      packageRoot: __dirname,
-        // contact for security and critical bug notices
-        configDir: "./greenlock.d",
-        maintainerEmail: 'tyler.hasenoehrl@getg5.com',
-        packageAgent: pkg.name + "/" + pkg.version,
-        // whether or not to run at cloudscale
-        cluster: false
-    })
-    // Serves on 80 and 443
-    // Get's SSL certificates magically!
-    .serve(app);
     console.log("Models Sync'd")
   })
   .catch(e => console.error(e))
+
+require("greenlock-express")
+  .init({
+    packageRoot: __dirname,
+    // contact for security and critical bug notices
+    configDir: "./greenlock.d",
+    maintainerEmail: 'tyler.hasenoehrl@getg5.com',
+    packageAgent: pkg.name + "/" + pkg.version,
+    // whether or not to run at cloudscale
+    cluster: false
+  })
+  // Serves on 80 and 443
+  // Get's SSL certificates magically!
+  .serve(app);
 
 // [SECURITY]
 // Since v2.4.0+ Greenlock proactively protects against
