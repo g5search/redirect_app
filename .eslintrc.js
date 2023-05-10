003cee2230982c8c485d05a351cd76cbfe754d90
David Miller <15317284+pcktbot@users.noreply.github.com>
2023-05-10 12:13:06 -0500

    lint support files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "jest": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2017
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: { ecmaVersion: 2017 },
+  rules: {
+    indent: ['error', 'tab'],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
 };
