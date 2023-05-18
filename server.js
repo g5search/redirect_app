724f33cb9a716399c37f7b90226a354f949b1a91
David Miller <15317284+pcktbot@users.noreply.github.com>
2023-05-18 16:17:34 -0500

    desperate

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 require('dotenv').config();
-const { GREENLOCK_MAINTAINER_EMAIL, GREENLOCK_DIR } = process.env;
+// const { GREENLOCK_MAINTAINER_EMAIL, GREENLOCK_DIR } = process.env;
 
-const greenlock = require('@root/greenlock-express');
+// const greenlock = require('@root/greenlock-express');
 const app = require('./app/lib/index.js');
 const models = require('./app/models');
-const pkg = require('./package.json');
+// const pkg = require('./package.json');
 
 models.sequelize
   .sync()
   .then(() => { console.info('Database schema synced!'); })
   .catch(e => console.error(e));
 
-greenlock
-  .init({
-    packageRoot: __dirname,
-    configDir: GREENLOCK_DIR,
-    maintainerEmail: GREENLOCK_MAINTAINER_EMAIL,
-    packageAgent: `${pkg.name}/${pkg.version}`,
-    cluster: false
-  })
-  .serve(app);
+app.listen(process.env.PORT, () => {
+  console.info(`Listening on port ${process.env.PORT}`);
+});
+
+// greenlock
+//   .init({
+//     packageRoot: __dirname,
+//     configDir: GREENLOCK_DIR,
+//     maintainerEmail: GREENLOCK_MAINTAINER_EMAIL,
+//     packageAgent: `${pkg.name}/${pkg.version}`,
+//     cluster: false
+//   })
+//   .serve(app);
