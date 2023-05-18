b8553d02de83e541a62301a3b96a1a8283fd74cc
David Miller <15317284+pcktbot@users.noreply.github.com>
2023-05-18 16:24:40 -0500

    force await

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 require('dotenv').config();
-// const { GREENLOCK_MAINTAINER_EMAIL, GREENLOCK_DIR } = process.env;
+const { GREENLOCK_MAINTAINER_EMAIL, GREENLOCK_DIR } = process.env;
 
-// const greenlock = require('@root/greenlock-express');
+const greenlock = require('@root/greenlock-express');
 const app = require('./app/lib/index.js');
 const models = require('./app/models');
-// const pkg = require('./package.json');
+const pkg = require('./package.json');
 
-models.sequelize
-  .sync()
-  .then(() => { console.info('Database schema synced!'); })
-  .catch(e => console.error(e));
+(async () => {
+  await models.sequelize
+    .sync()
+    .then(() => { console.info('Database schema synced!'); })
+    .catch(e => console.error(e));
 
-app.listen(process.env.PORT, () => {
-  console.info(`Listening on port ${process.env.PORT}`);
-});
-
-// greenlock
-//   .init({
-//     packageRoot: __dirname,
-//     configDir: GREENLOCK_DIR,
-//     maintainerEmail: GREENLOCK_MAINTAINER_EMAIL,
-//     packageAgent: `${pkg.name}/${pkg.version}`,
-//     cluster: false
-//   })
-//   .serve(app);
+  greenlock
+    .init({
+      packageRoot: __dirname,
+      configDir: GREENLOCK_DIR,
+      maintainerEmail: GREENLOCK_MAINTAINER_EMAIL,
+      packageAgent: `${pkg.name}/${pkg.version}`,
+      cluster: false
+    })
+    .serve(app);
+})();
