3ea34cd454de0e5266466d2af800d9824c2981a9
David Miller <15317284+pcktbot@users.noreply.github.com>
2023-05-18 14:32:21 -0500

    move nws external symlink

--- chart/charts/node-web-service/README.md
+++
@@ -1,3 +0,0 @@
-## `node-web-service`
-
-A Helm chart for deploying our Node apps. It brings in other charts as necessary to install infrastructure like CloudSQL, Memorystore, and Pub/Sub.
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const pkg = require('./package.json');
 
 models.sequelize
   .sync()
-  // .then(() => { console.info('Database schema synced!'); })
+  .then(() => { console.info('Database schema synced!'); })
   .catch(e => console.error(e));
 
 greenlock
