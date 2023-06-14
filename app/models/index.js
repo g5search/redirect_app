0c2fbc4be94195390ddf62b9a3e7c1d6bc130746
David Miller <15317284+pcktbot@users.noreply.github.com>
2023-06-14 13:17:38 -0500

    don't log

--- app/models/index.js
+++ app/models/index.js
@@ -20,17 +20,6 @@ const {
 
 const DATABASE_URL_ENV = process.env.NODE_ENV === 'test' ? DATABASE_URL_TEST : DATABASE_URL;
 
-console.info({
-  DATABASE_URL_ENV,
-  DATABASE_URL,
-  DATABASE_URL_TEST,
-  DATABASE_MAX_CONNECTIONS,
-  DATABASE_PASSWORD,
-  DATABASE_HOST,
-  DATABASE_NAME,
-  DATABASE_USER
-});
-
 const sequelize = new Sequelize(DATABASE_URL_ENV, {
   pool: {
     max: parseInt(DATABASE_MAX_CONNECTIONS),
